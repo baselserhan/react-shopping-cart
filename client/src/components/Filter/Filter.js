3e005e6887af3034597dcfd852464673d9cfbf86
Basel Serhan <baselserhan1993@gmail.com>
2023-02-08 14:18:03 +0200

    filter comp

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -8,21 +8,27 @@ function Filter(props) {
       <div className="num-of-products">Number of Products 4</div>
       <div className="filter-by-size">
         <span>Filter</span>
-        <select className="filter-select">
+        <select
+          className="filter-select"
+          value={props.size}
+          onChange={props.handleFilterBySize}
+        >
           <option value="ALL">ALL</option>
           <option value="XS">XS</option>
           <option value="S">S</option>
           <option value="M">M</option>
           <option value="L">L</option>
-          <option value="XL">XL</option>
-          <option value="XXL">XXL</option>
         </select>
       </div>
       <div className="filter-by-size">
         <span>Order</span>
-        <select className="filter-select">
+        <select
+          className="filter-select"
+          value={props.sort}
+          onChange={props.handleFilterByOrder}
+        >
           <option value="latest">Latest</option>
-          <option value="lower">Lower</option>
+          <option value="lowest">Lowest</option>
           <option value="highest">Highest</option>
         </select>
       </div>
