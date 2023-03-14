e52cad29c7411d44997d797ac387f6518876f5bd
Basel Serhan <baselserhan1993@gmail.com>
2023-03-14 12:54:26 +0200

    add redux

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../css/Products/Products.css";
 import ProductModal from "./ProductModal";
 import Bounce from "react-reveal/Bounce";
+import { connect } from "react-redux";
+import { fetchProducts } from "../../store/actions/products";
 
 function Products(props) {
   const [product, setProduct] = useState("");
@@ -13,23 +15,29 @@ function Products(props) {
   const closeModal = () => {
     setProduct(false);
   };
+
+  useEffect(() => {
+    props.fetchProducts();
+  }, []);
   return (
     <Bounce left cascade>
       <div className="products-wrapper">
-        {props.products.map((product) => (
-          <div className="product-item" key={product.id}>
-            <a href="#" onClick={() => openModal(product)}>
-              <img src={product.imageUrl} alt={product.title} />
-            </a>
-            <div className="product-desc">
-              <p>{product.title}</p>
-              <span>${product.price}</span>
-            </div>
-            <button onClick={() => props.addToCart(product)}>
-              Add To Cart
-            </button>
-          </div>
-        ))}
+        {props.products && props.products.length
+          ? props.products.map((product) => (
+              <div className="product-item" key={product.id}>
+                <a href="#" onClick={() => openModal(product)}>
+                  <img src={product.imageUrl} alt={product.title} />
+                </a>
+                <div className="product-desc">
+                  <p>{product.title}</p>
+                  <span>${product.price}</span>
+                </div>
+                <button onClick={() => props.addToCart(product)}>
+                  Add To Cart
+                </button>
+              </div>
+            ))
+          : "loading.."}
 
         <ProductModal product={product} closeModal={closeModal} />
       </div>
@@ -37,4 +45,11 @@ function Products(props) {
   );
 }
 
-export default Products;
+export default connect(
+  (state) => {
+    return {
+      products: state.products.products,
+    };
+  },
+  { fetchProducts }
+)(Products);
